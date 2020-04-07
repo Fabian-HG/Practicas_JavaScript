3e431a7d0382972d2f845c0292b9a8ee0d387a8f
José Fabián Hernández García <fabi-fabian@live.com>
2020-04-07 15:44:16 -0500

    Modificación para revisar el error

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,5 +1,7 @@
-// Al cargarse el archivo JavaScript, se muestra un mensaje
-alert("Hola Mundo!");
+function invocarScript() {
+    // Al cargarse el archivo JavaScript, se muestra un mensaje
+    alert("Hola Mundo!");
 
-// Despues del primer mensaje, se muestra otro mensaje seguido
-alert("Soy el primer script");
+    // Despues del primer mensaje, se muestra otro mensaje seguido
+    alert("Soy el primer script");
+}
